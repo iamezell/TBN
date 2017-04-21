95a24da7ecc3d2c2fc2433104d1dad2a53fa1826
Ezell <iamezell@gmail.com>
2017-04-21 10:03:20 -0400

    feature(): adding expo mobile

--- mobile/main.js
+++ mobile/main.js
@@ -1,13 +1,12 @@
+import Expo from 'expo';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
-export default class App extends React.Component {
+class App extends React.Component {
   render() {
     return (
       <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-        <Text>Changes you make will automatically reload.</Text>
-        <Text>Shake your phone to open the developer menu.</Text>
+        <Text>Open up main.js to start working on your app!</Text>
       </View>
     );
   }
@@ -21,3 +20,5 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
   },
 });
+
+Expo.registerRootComponent(App);
